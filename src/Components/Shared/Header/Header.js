96b3140adc600f7022fe5e929e902c941ff6cfe0
Md-Nihal <mdnihal01716@gmail.com>
2022-06-04 21:21:36 +0600

    some edit

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
-import photo from '../../../assets/images/portfolio.jpg'
+
 
 const Header = () => {
     const [user] = useAuthState(auth);
@@ -50,11 +50,7 @@ const Header = () => {
                 <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                        <img src={photo} alt=""/>
-                    </div>
-                </label>
+               
             </div>
         </div>
     );
